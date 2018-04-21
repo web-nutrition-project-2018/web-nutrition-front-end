366914c32b38a04616432396ccdd5c5c19f129e1
Vincentius Kevin <kevincentius@yahoo.com>
2018-04-21 05:18:55 +0200

    Bar heights and description from server

--- popup.js
+++ popup.js
@@ -6,9 +6,12 @@ $(function(){
     // get the URL currently opened tab
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
         var url = tabs[0].url;
-        //$.get( "http://www.this.should.be.our.server.address.com", http_params, function( data ) {
-            // mock object (since server is not implemented)
-            var chartData = [
+        var http_params = {
+            "url": url
+        };
+        $.get( "http://localhost:8081/nutrition", http_params, function( data ) {
+            // mock object (for testing, if server is not available)
+            /*var chartData = [
                 {
                     "name": "effort",
                     "display": "effort",
@@ -39,33 +42,40 @@ $(function(){
                     "value": 12,
                     "color": "#00f"
                 }
-            ];
+            ];*/
+
+            console.log(data);
 
             // actual code updating the UI
-            chartData.forEach(col => {
-                $('#nutrition_chart').append(`
-                    <div class="nutrition-bar" id="bar_${col.name}" tabindex="0">
-                        <div class="nutrition-track">
-                            <div class="nutrition-fill" id="fill_${col.name}">
-                                <span>${col.value}</span>
+            $('#nutrition_loading').hide();
+            if (data.error != null) {
+                $('#nutrition_explanation').text(data.error);
+            } else {
+                data.nutrition.forEach(col => {
+                    $('#nutrition_chart').append(`
+                        <div class="nutrition-bar" id="bar_${col.name}" tabindex="0">
+                            <div class="nutrition-track">
+                                <div class="nutrition-fill" id="fill_${col.name}">
+                                    <span>${Math.round(col.value)}</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                `);
-                
-                $('#fill_' + col.name).css({
-                    "height": col.value + "%",
-                    "top": (100 - col.value) + "%",
-                    "background": col.color
-                });
+                    `);
+                    
+                    $('#fill_' + col.name).css({
+                        "height": col.percentage + "%",
+                        "top": (100 - col.percentage) + "%",
+                        "background": col.color
+                    });
 
-                $('#bar_' + col.name).hover(e => { updateExplanation(col); });
-                $('#bar_' + col.name).click(e => { updateExplanation(col); });
-            });
-        //});
+                    $('#bar_' + col.name).hover(e => { updateExplanation(col.display); });
+                    $('#bar_' + col.name).click(e => { updateExplanation(col.display); });
+                });
+            }
+        });
     });
 });
 
-function updateExplanation(col) {
-    $('#nutrition_explanation').text(col.name + ': ' + col.value + '%');
+function updateExplanation(text) {
+    $('#nutrition_explanation').text(text);
 }
