425d39abbce6825b13c2f4cd26270ca9e5014645
unknown <kevincentius@yahoo.com>
2018-04-22 14:15:56 -0700

    mock data

--- popup.js
+++ popup.js
@@ -9,9 +9,9 @@ $(function(){
         var http_params = {
             "url": url
         };
-        $.get( "http://localhost:8081/nutrition", http_params, function( data ) {
+        //$.get( "http://localhost:8081/nutrition", http_params, function( data ) {
             // mock object (for testing, if server is not available)
-            //var data = {"url": "https://stackoverflow.com/questions/4630704/receiving-fatal-not-a-git-repository-when-attempting-to-remote-add-a-git-repo", "nutrition": [{"name": "readability", "display": "readability: 61%", "value": 61.26, "percentage": 61.26, "color": "#f00"}, {"name": "flesch_kincaid_grade", "display": "flesch kincaid grade: 9", "value": 9.3, "percentage": 53.49999999999999, "color": "#fc0"}, {"name": "dale_chall_readability_score", "display": "dale chall readability: 8", "value": 7.83, "percentage": 34.75, "color": "#0f0"}, {"name": "linsear_write_formula", "display": "linsear write formula: 8", "value": 8.333333333333334, "percentage": 58.33333333333333, "color": "#0cc"}, {"name": "gunning_fog", "display": "gunning fog: 18", "value": 17.676831683168317, "percentage": 41.07722772277228, "color": "#00f"}]};
+            var data = {"url": "https://stackoverflow.com/questions/4630704/receiving-fatal-not-a-git-repository-when-attempting-to-remote-add-a-git-repo", "nutrition": [{"name": "readability", "display": "readability: 61%", "value": 61.26, "percentage": 61.26, "color": "#f00"}, {"name": "flesch_kincaid_grade", "display": "flesch kincaid grade: 9", "value": 9.3, "percentage": 53.49999999999999, "color": "#fc0"}, {"name": "dale_chall_readability_score", "display": "dale chall readability: 8", "value": 7.83, "percentage": 34.75, "color": "#0f0"}, {"name": "linsear_write_formula", "display": "linsear write formula: 8", "value": 8.333333333333334, "percentage": 58.33333333333333, "color": "#0cc"}, {"name": "gunning_fog", "display": "gunning fog: 18", "value": 17.676831683168317, "percentage": 41.07722772277228, "color": "#00f"}]};
 
             console.log(data);
 
@@ -41,7 +41,7 @@ $(function(){
                     $('#bar_' + col.name).click(e => { updateExplanation(col.display); });
                 });
             }
-        });
+        //});
     });
 });
 
